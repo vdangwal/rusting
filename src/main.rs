af3c51bcf4519c66bcddb099bab7f32ec109f407
Vikas Dangwal <ourmac@Vikass-MBP.home>
2019-06-04 22:44:43 -0400

    secret number guess

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+extern crate rand;
+
+use rand::Rng;
 use std::io;
 fn main() {
-    println!("Please type something and then hit enter");
+    println!("Please guess a number between 1 and 100");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .ok()
         .expect("Failed To Read Line");
-    println!("You Guessed : {}", guess);
+    println!(
+        "You Guessed {} and secret number was {}",
+        guess, secret_number
+    );
 }
 

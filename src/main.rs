df9e8971b3c7919ec526cbf092ea6c94ef481153
Vikas Dangwal <ourmac@Vikass-MBP.home>
2019-06-24 22:49:25 -0400

    tested with vectors and thread

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 extern crate rand;
 
-use rand::Rng;
-use std::io;
-fn main() {
-    println!("Please guess a number between 1 and 100");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+use http::{Request, Response};
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .ok()
-        .expect("Failed To Read Line");
-    println!(
-        "You Guessed {} and secret number was {}",
-        guess, secret_number
-    );
+use std::thread;
+use std::time::SystemTime;
+fn main() {
+    process_http_requests();
+    // println!("Please guess a number between 1 and 100");
+    // let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // let mut guess = String::new();
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .ok()
+    //     .expect("Failed To Read Line");
+    // println!(
+    //     "You Guessed {} and secret number was {}",
+    //     guess, secret_number
+    // );
 }
 
+fn process_http_requests() {
+    let mut vect_of_names = Vec::new();
+    vect_of_names.push("Vikas");
+    vect_of_names.push("Diksha");
+    vect_of_names.push("Viyona");
+
+
+    for i in vect_of_names {
+        thread::spawn(move || {
+            print!("This is the Value of i {} \n at {:?}", i, SystemTime::now());
+        });
+    }
+}
